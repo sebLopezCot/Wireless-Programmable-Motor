9a89d4ade8296ea6abead1cfee75ad612cbc1989
sebLopezCot <sclopez2@hotmail.com>
2015-04-06 21:34:31 -0400

    Fixed bugs in StepperMotor code

--- Arduino/Wireless_Programmable_Motor_Firmware/WirelessProgrammableMotorLibraries/StepperMotor.cpp
+++ Arduino/Wireless_Programmable_Motor_Firmware/WirelessProgrammableMotorLibraries/StepperMotor.cpp
@@ -4,7 +4,9 @@ StepperMotor::StepperMotor(int directionPin, int stepPin){
 	MOTOR_DIRECTION_PIN = directionPin;
 	MOTOR_STEP_PIN = stepPin;
 	MOTOR_DELAY_INCREMENT = TOP_SPEED_DELAY_TIME;
+}
 
+void StepperMotor::init(){
 	// Setup the digital outputs for the stepper motor
   	pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
   	pinMode(MOTOR_STEP_PIN, OUTPUT);
@@ -25,18 +27,17 @@ void StepperMotor::setDirection(int direction){
 }
 
 void StepperMotor::spinForDegrees(double degrees){
-	int stepsNeeded = (int)(STEPS_PER_REV * degrees / 360.0);
+	int stepsNeeded = (int)(MICROSTEPS_PER_REV * degrees / 360.0);
 	spinForSteps(stepsNeeded);
 }
 
 void StepperMotor::spinForRadians(double rads){
-	double PI = 3.1415926;
-	int stepsNeeded = (int)(STEPS_PER_REV * rads / (2 * PI));
+	int stepsNeeded = (int)(MICROSTEPS_PER_REV * rads / (2 * 3.1415926));
 	spinForSteps(stepsNeeded);
 }
 
 void StepperMotor::spinForRevs(double revs){
-	int stepsNeeded = (int)(STEPS_PER_REV * revs);
+	int stepsNeeded = (int)(MICROSTEPS_PER_REV * revs);
 	spinForSteps(stepsNeeded);
 }
 
@@ -44,13 +45,13 @@ void StepperMotor::spinForSteps(int steps){
 	// Set the direction of motion
 	if(steps > 0){
 		setDirection(CW);
-	} else {
+	} else if (steps < 0) {
 		setDirection(CCW);
 	}
 
 	// Move for certain number of steps
 	int i=0;
-	for(i=0; i < steps; i++){
+	for(i=0; i < abs(steps); i++){
 		digitalWrite(MOTOR_STEP_PIN, LOW);  // This LOW to HIGH change is what creates the
     	digitalWrite(MOTOR_STEP_PIN, HIGH); // "Rising Edge" so the easydriver knows to when to step.
     	delayMicroseconds(MOTOR_DELAY_INCREMENT);
--- Arduino/Wireless_Programmable_Motor_Firmware/WirelessProgrammableMotorLibraries/StepperMotor.h
+++ Arduino/Wireless_Programmable_Motor_Firmware/WirelessProgrammableMotorLibraries/StepperMotor.h
@@ -6,6 +6,8 @@
 class StepperMotor{
 public:
 	StepperMotor(int directionPin, int stepPin);
+	
+	void init();
 
 	void setMotorDelayIncrement(int delay);
 
@@ -27,6 +29,8 @@ private:
 
 	static const int TOP_SPEED_DELAY_TIME = 200; // milliseconds
 	static const int STEPS_PER_REV = 400;
+	static const int MICROSTEPS_PER_STEP = 8;	// In default 1/8th microstep mode
+	static const int MICROSTEPS_PER_REV = STEPS_PER_REV * MICROSTEPS_PER_STEP;
 
 	void setDirection(int direction);
 };
